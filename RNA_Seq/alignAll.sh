d04a344f383cb18c513880f4652427235114ea88
gkjhgkjh <ying.x@husky.neu.edu>
2017-09-27 23:07:31 -0400

    edited on server

--- RNA_Seq/alignAll.sh
+++ RNA_Seq/alignAll.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-fastqPath="trimmed/"
-for f1 in $fastqPath*.R1.paired.fastq
+fastqPath="/scratch/AiptasiaMiSeq/fastq/"
+trimPath="trimmed/"
+for f1 in $fastqPath*.R1.fastq
 do
         f2=$(echo $f1 |cut -d'.' -f1)
         num0cc=$(tr -dc '/' <<<"$f2" | awk '{ print length; }')
         ((num0cc ++))
         f3=$(echo $f2 |cut -d'/' -f$num0cc)
-        echo \
 	nice -n 19 gsnap \
 	-A sam \
 	-s AiptasiaGmapIIT.iit \
 	-D . \
 	-d AiptasiaGmapDb \
-	$fastqPath$f3.R1.paired.fastq \
-	$fastqPath$f3.R2.paired.fastq \
-	1>$3.sam 2>$3.err &
+	$trimPath$f3.R1.paired.fastq \
+	$trimPath$f3.R2.paired.fastq \
+	1>$f3.sam 2>$f3.err &
 done
