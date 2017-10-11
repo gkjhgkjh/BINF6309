88f8fc34da021b36410140c7f112129a8f6c42c9
gkjhgkjh <ying.x@husky.neu.edu>
2017-10-10 20:31:27 -0400

    edited on mac

--- BioPerl/test.pl
+++ BioPerl/test.pl
@@ -3,29 +3,11 @@ use warnings;
 use strict;
 use diagnostics;
 
-use Bio::Seq;
-use Bio::SeqIO;
-use Bio::Seq::Quality;
-use Getopt::Long;
-use Pod::Usage;
 
-#set variables for later input
-my $qual = 0;
-my $usage = "\n$0 [options] \n
-Options:
-	-qual	Quality score minimum
-	-help	Show this message
-\n";
-#set options
-GetOptions(
-	'qual=i' => \$qual,
-	help => sub {pod2usage($usage);},
-	
-) or pod2usage($usage);
-#set warnings for file check
-unless ($qual) {
-		print "Specify quality score cutoff\n", $usage;
-		}
-		die $usage;
-		
-print($qual);
+my $s=2;
+
+sub check {
+ print($s-1);
+}
+
+check()
