cec62ba047e9156cfc218dd2ece45bf23b4eb3be
gkjhgkjh <ying.x@husky.neu.edu>
2017-10-11 17:52:41 -0400

    edited on server

--- RNA_Seq/trinityDeNovo.sh
+++ RNA_Seq/trinityDeNovo.sh
@@ -2,8 +2,11 @@
 leftReads="$(ls -q Paired/*.R1.fastq)"
 leftReads=$(echo $leftReads)
 leftReads="${leftReads// /,}"
-echo $leftReads
 rightReads="$(ls -q Paired/*.R2.fastq)"
 rightReads=$(echo $rightReads)
 rightReads="${rightReads// /,}"
-echo $rightReads
+nice -n19 /usr/local/programs/\
+trinityrnaseq-2.2.0/\
+Trinity --seqType fq --max_memory 50G --output trinity_de-novo \
+--left $leftReads --right $rightReads --CPU 4 \
+1>trinity.log 2>trinity.err $
