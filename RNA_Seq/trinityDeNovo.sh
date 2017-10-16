01110d05713226a9e892ad8cf23b5a37aa2db1dc
gkjhgkjh <ying.x@husky.neu.edu>
2017-10-16 19:42:57 -0400

    edited on mac

--- RNA_Seq/trinityDeNovo.sh
+++ RNA_Seq/trinityDeNovo.sh
@@ -9,4 +9,4 @@ nice -n19 /usr/local/programs/\
 trinityrnaseq-2.2.0/\
 Trinity --seqType fq --max_memory 50G --output trinity_de-novo \
 --left $leftReads --right $rightReads --CPU 4 \
-1>trinity.log 2>trinity.err $
+1>trinity.log 2>trinity.err &
