81fc69560c8cee37d084d8fcdd9d6337b5eb8906
gkjhgkjh <ying.x@husky.neu.edu>
2017-11-13 16:22:26 -0500

    edited on mac

--- RNA_Seq/selectedGenes.R
+++ RNA_Seq/selectedGenes.R
@@ -1,9 +1,14 @@
 rm(list=ls())
+#Load Summarized DE Results
 load("summarizedDeResults.RData")
+#Read Annotation Files
 spAnnot <- read.delim("proteinGeneGo.txt", sep="\t", header=FALSE)
+#Set Column Names
 colnames(spAnnot) = c("Gene", "Protein", "GeneDesc", "GO")
+#Select Desired Columns
 goDesc <- subset(spAnnot, select=c(GO, Gene))
 
+#Find Immune-Related
 immune <- goDesc[grep("immune", goDesc$GO),]
 immuneGenes<-unique(immune$Gene)
 immuneDE <- dfBothAnnot[row.names(dfBothAnnot) %in% immuneGenes, ]
