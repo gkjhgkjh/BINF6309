d4f813ad3743babfad7c143140042b8a1bd37157
gkjhgkjh <ying.x@husky.neu.edu>
2017-11-15 13:28:12 -0500

    edited on server

--- GenomeAssembly/trim.sh
+++ GenomeAssembly/trim.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 nice -n 19 java -jar /usr/local/programs/Trimmomatic-0.36/trimmomatic-0.36.jar PE \
 -threads 1 -phred33 \
-/scratch/AiptasiaMiSeq/fastq/Aip02.R1.fastq \
-/scratch/AiptasiaMiSeq/fastq/Aip02.R2.fastq \
-Aip02.R1.paired.fastq \
-Aip02.R1.unpaired.fastq \
-Aip02.R2.paired.fastq \
-Aip02.R2.unpaired.fastq \
+SRR6224333_1.fastq \
+SRR6224333_2.fastq \
+SRR6224333.R1.paired.fastq \
+SRR6224333.R1.unpaired.fastq \
+SRR6224333.R2.paired.fastq \
+SRR6224333.R2.unpaired.fastq \
 HEADCROP:0 \
 ILLUMINACLIP:/usr/local/programs/Trimmomatic-0.36/adapters/NexteraPE-PE.fa:2:30:10 \
 LEADING:20 TRAILING:20 SLIDINGWINDOW:4:30 MINLEN:36 \
-1>Aip02.trim.log 2>Aip02.trim.err &
+1>SRR6224333.trim.log 2>SRR6224333.trim.err &
