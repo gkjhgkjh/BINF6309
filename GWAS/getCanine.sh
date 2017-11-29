235803d569ac1b1624d9e6f24a618365575b5623
gkjhgkjh <ying.x@husky.neu.edu>
2017-11-29 17:45:46 -0500

    edited on server

---
+++ GWAS/getCanine.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Get the Canine PLINK Data
+wget http://datadryad.org/bitstream/handle/10255/dryad.104772/cornell_canine.fam
+wget http://datadryad.org/bitstream/handle/10255/dryad.104773/cornell_canine.bim
+wget http://datadryad.org/bitstream/handle/10255/dryad.104774/cornell_canine.bed
+wget http://datadryad.org/bitstream/handle/10255/dryad.104775/phenotypes.txt
