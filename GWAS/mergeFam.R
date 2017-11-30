dad8b2d8293bb716bf874329611d6402b3e1d92d
gkjhgkjh <ying.x@husky.neu.edu>
2017-11-29 23:05:54 -0500

    edtied on server

--- GWAS/mergeFam.R
+++ GWAS/mergeFam.R
@@ -1,6 +1,7 @@
-fam <- read.table(cornell_canine.fam)
-colnames(dogID, inFam, inFamF, inFamM, sex, epilepsy_irishWolfhounds)
-pheno <- read.table(phenotypes.txt)
+#!/usr/bin/Rscript
+fam <- read.table("cornell_canine.fam")
+colnames(fam)<-c("dogID", "inFam", "inFamF", "inFamM", "sex", "pheno")
+pheno <- read.table("phenotypes.txt", header = T)
 famPheno<-merge(fam,pheno)
 famPheno<- subset(famPheno, select=c(dogID, inFam, inFamF, inFamM, sex, epilepsy_irishWolfhounds))
 write.table(famPheno, file = "famEpilepsy.fam", col.names = F, row.names = F, quote = F)
