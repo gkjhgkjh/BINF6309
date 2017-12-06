b43b38fc94256b26df6be7c463bcc0207f9b277b
gkjhgkjh <ying.x@husky.neu.edu>
2017-12-06 18:24:19 -0500

    edited on mac

--- GWAS/selectSig.R
+++ GWAS/selectSig.R
@@ -3,8 +3,9 @@ ep<-read.table("epilepsy.txt.assoc.adjusted", header = T)
 ep1<-read.table("epilepsy.txt.assoc", header = T)
 row<-NULL
 for(i in 1:160170){
-  if(ep[i,5]<=0.5){
+  if(ep[i,5]<=0.05){
     row <- append(row,i)
   }
 }
+write.csv(ep[row,], "Bonadj.csv")
 
