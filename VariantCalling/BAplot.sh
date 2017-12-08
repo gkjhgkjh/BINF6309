2392c0f3357ae8b746005738afdb0a800d5f8741
gkjhgkjh <ying.x@husky.neu.edu>
2017-12-07 23:49:46 -0500

    edited on server

--- VariantCalling/BAplot.sh
+++ VariantCalling/BAplot.sh
@@ -8,4 +8,4 @@ nice -n19 java -jar /usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.
 -before recal_data.table \
 -after post_recal_data.table \
 -plots recalibration_plots.pdf \
-1>plot.log 2>plot.err
+1>plot.log 2>plot.err &
