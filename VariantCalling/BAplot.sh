b0b7433250c573b9619fe9b0394797aef10de5bd
gkjhgkjh <ying.x@husky.neu.edu>
2017-12-10 23:56:25 -0500

    edited on server

--- VariantCalling/BAplot.sh
+++ VariantCalling/BAplot.sh
@@ -3,7 +3,6 @@
 fa='vShiloni.fasta'
 nice -n19 java -jar /usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar \
 -T AnalyzeCovariates \
--nct 16 \
 -R $fa \
 -before recal_data.table \
 -after post_recal_data.table \
